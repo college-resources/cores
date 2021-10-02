2f948bdc8323de4b0a34e1154b97cda4e07c58bc
jim3692 <jim3692@gmail.com>
2021-10-02 16:01:35 +0300

    Restore session changes

--- web/server/middleware/session.js
+++ web/server/middleware/session.js
@@ -3,6 +3,12 @@ const {FirestoreStore} = require('@google-cloud/connect-firestore')
 const {Firestore} = require('@google-cloud/firestore')
 
 const sess = {
+  store: process.env.NODE_ENV === 'production'
+    ? new FirestoreStore({
+      dataset: new Firestore(),
+      kind: process.env.SESSION_COLLECTION || 'local-sessions',
+    })
+    : undefined,
   cookie: {},
   resave: false,
   saveUninitialized: true,
